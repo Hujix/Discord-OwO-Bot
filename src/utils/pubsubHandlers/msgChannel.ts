3989af857a897c3c8f9b132294d530e70c300ce3
Skillz <gankstarsleague@gmail.com>
2020-01-25 09:45:50 -0500

    msgchannel to ts

--- src/utils/pubsubHandlers/msgChannel.ts
+++ src/utils/pubsubHandlers/msgChannel.ts
@@ -1,13 +1,15 @@
+import OwO from '../../owo'
+
 /*
  * OwO Bot for Discord
  * Copyright (C) 2019 Christopher Thai
  * This software is licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
  * For more information, see README.md and LICENSE
-  */
+ */
 
-exports.handle = async function(main, message){
-	let {channelID,msg} = JSON.parse(message);
-	if(!main.bot.channelGuildMap[channelID]) return;
-	main.bot.createMessage(channelID,msg);
+export default (main: OwO, message: string) => {
+  const { channelID, msg } = JSON.parse(message)
+  if (!main.bot.channels.has(channelID)) return
+
+  main.bot.createMessage(channelID, msg)
 }
-	
