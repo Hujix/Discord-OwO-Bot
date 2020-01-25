681a307dd5526b6195abc69eac063b682aa524d2
Skillz <gankstarsleague@gmail.com>
2020-01-25 09:17:49 -0500

    end process to typescript

---
+++ src/utils/pubsubHandlers/endProcess.ts
@@ -0,0 +1,21 @@
+import OwO from '../../owo'
+
+/*
+ * OwO Bot for Discord
+ * Copyright (C) 2019 Christopher Thai
+ * This software is licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ * For more information, see README.md and LICENSE
+ */
+
+let timer: NodeJS.Timeout | undefined
+
+export default async function(main: OwO) {
+  // If there was a previous attempt to restart then reset it
+  if (timer) clearTimeout(timer)
+
+  const time = main.clusterID * 60000
+  console.log(`ending ${main.clusterID} in ${time}ms`)
+  timer = setTimeout(function() {
+    process.exit(0)
+  }, time)
+}
