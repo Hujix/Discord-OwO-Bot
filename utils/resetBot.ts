b6b321901645bbf37e83e5885d20b5dff43ec692
Skillz <gankstarsleague@gmail.com>
2020-01-24 20:42:33 -0500

    refactor global

--- utils/resetBot.ts
+++ utils/resetBot.ts
@@ -3,9 +3,9 @@
  * Copyright (C) 2019 Christopher Thai
  * This software is licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
  * For more information, see README.md and LICENSE
-  */
-const pub = require('redis').createClient();
-	
-exports.resetBot = async function(){
-	pub.publish("endProcess",true);
-}
+ */
+import redis from 'redis'
+const pub = redis.createClient()
+
+/** Sends a request on pubsub to the 'endProcess' channel which will restart all shards. */
+export default () => pub.publish('endProcess', 'true')
