cb5623138ae78ddeec659cadba53125d7e7eb73a
Skillz <gankstarsleague@gmail.com>
2020-01-29 09:42:27 -0500

    comment unused parts for now

--- src/registry.ts
+++ src/registry.ts
@@ -2,10 +2,10 @@ import { Registry, Handler } from 'patron'
 import path from 'path'
 
 const registry = new Registry({ defaultReaders: true })
-  .registerPreconditions(path.join(__dirname, 'inhibitors/'))
-  .registerArgumentPreconditions(path.join(__dirname, 'validators/'))
-  .registerTypeReaders(path.join(__dirname, 'readers/'))
-  .registerPostconditions(path.join(__dirname, 'postconditions/'))
+  // .registerPreconditions(path.join(__dirname, 'inhibitors/'))
+  // .registerArgumentPreconditions(path.join(__dirname, 'validators/'))
+  // .registerTypeReaders(path.join(__dirname, 'readers/'))
+  // .registerPostconditions(path.join(__dirname, 'postconditions/'))
   .registerCommands(path.join(__dirname, 'commands/'))
 
 const handler = new Handler({ registry })
