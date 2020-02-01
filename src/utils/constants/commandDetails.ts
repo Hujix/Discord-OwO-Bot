feee16ca1af07640058e26b1f8da6a07793311a5
Skillz <gankstarsleague@gmail.com>
2020-02-01 13:57:44 -0500

    command details file

---
+++ src/utils/constants/commandDetails.ts
@@ -0,0 +1,31 @@
+export const commandDetails = [
+  {
+    name: 'avatar',
+    examples: ['owo avatar @OwO'],
+    half: 100,
+    six: 500
+  },
+  {
+    name: 'guildlink',
+    related: ['owo link'],
+    half: 100,
+    six: 500
+  },
+  {
+    name: 'help',
+    examples: ['owo help cowoncy', 'owo help'],
+    half: 100,
+    six: 500
+  },
+  {
+    name: 'invite',
+    related: ['owo guildlink'],
+    half: 100,
+    six: 500
+  },
+  {
+    name: 'merch',
+    half: 100,
+    six: 500
+  }
+]
