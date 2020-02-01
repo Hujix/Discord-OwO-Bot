39fd0813d6c3eacef2cc9abbc4187b14060dbe9d
Skillz <gankstarsleague@gmail.com>
2020-01-31 22:48:14 -0500

    more constants

--- src/utils/constants/emojis.ts
+++ src/utils/constants/emojis.ts
@@ -1,7 +1,9 @@
 export default {
   blank: '<:blank:427371936482328596>',
+  leftArrow: '⬅️',
   lift: '🙇',
+  owo: '<:owo:448256976141549568>',
+  rightArrow: '➡️',
   skull: '☠',
-  timer: '⏱',
-  owo: '<:owo:448256976141549568>'
+  timer: '⏱'
 }
--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -3,5 +3,9 @@ import permissions from './permissions'
 
 export default {
   emojis,
-  permissions
+  permissions,
+  supportServerID: '420104212895105044',
+  patreonRoleID: '',
+  patreonDailyID: '449429399217897473',
+  patreonAnimalID: '449429255781351435'
 }
