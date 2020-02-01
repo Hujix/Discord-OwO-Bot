bad93b8a7a0b12509bce29c46320f04a0fc2e052
Skillz <gankstarsleague@gmail.com>
2020-02-01 13:57:53 -0500

    add battlehelppages as a constant

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -7,5 +7,14 @@ export default {
   supportServerID: '420104212895105044',
   patreonRoleID: '',
   patreonDailyID: '449429399217897473',
-  patreonAnimalID: '449429255781351435'
+  patreonAnimalID: '449429255781351435',
+  battleHelpPages: [
+    'https://i.imgur.com/cJ9F3DM.png',
+    'https://i.imgur.com/uHyENGL.png',
+    'https://i.imgur.com/QjYt5Xv.png',
+    'https://i.imgur.com/mFWT5wg.png',
+    'https://i.imgur.com/G3zfrc6.png',
+    'https://i.imgur.com/9LXG4qy.png',
+    'https://i.imgur.com/KGAcFZW.png'
+  ]
 }
