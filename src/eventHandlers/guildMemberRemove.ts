9123bbb680a7700159ce3bb8d64eb3e8e5b8a4be
Skillz <gankstarsleague@gmail.com>
2020-01-31 22:51:54 -0500

    remove obvious comments

--- src/eventHandlers/guildMemberRemove.ts
+++ src/eventHandlers/guildMemberRemove.ts
@@ -13,11 +13,9 @@ module.exports = {
   handle: async (guild: Guild, member: Member) => {
     if (guild.id !== constants.supportServerID || !member.roles.includes(constants.patreonRoleID)) return
 
-    // Update the database
     mysql.con.query(`UPDATE IGNORE user SET patreonDaily = 0,patreonAnimal = 0 WHERE id = ${member.id};`)
 
     try {
-      // Alert the user in dm if possible that they will lose the patreon benefits
       const dmChannel = await member.user.getDMChannel()
       await dmChannel.createMessage('Just a heads up! Your Patreon benefits will not work if you leave the guild!')
     } catch {}
